804434d88da0db73ad32a1bc560f8369d55a1f51
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-07-10 04:44:11 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -24,19 +24,19 @@ function switchAge(age) {
         break;
       case 14 :
         return "You are a teenager!";
-        break;  
+        break;
       case 15 :
         return "You are a teenager!";
-        break;  
+        break;
       case 16 :
         return "You are a teenager!";
         break;
       case 17 :
         return "You are a teenager!";
-        break;  
+        break;
       case 18 :
         return "You are a teenager!";
-        break;    
+        break;
       case 19 :
         return "You are a teenager!";
         break;
