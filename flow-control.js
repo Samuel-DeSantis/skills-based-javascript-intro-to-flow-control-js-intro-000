b3cc855e9b9a1fa73b55f46ba4c9f2540059bd70
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-07-10 04:45:03 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -8,7 +8,7 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  if (age < 12) {return "You are a kid";}
+  if (age < 13) {return "You are a kid";}
   else if (age > 12 && age < 20) {return "You are a teenager!";}
   else {return "You are grownup;"}
 }
